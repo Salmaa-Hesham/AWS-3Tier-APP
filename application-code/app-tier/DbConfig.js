8c8e5952dc767cda877a0f4635c196ffbaba2b7d
Salmaa-Hesham <salmahesham1114@gmail.com>
2025-04-14 01:49:01 +0200

    source code files

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST: 'project-3tier-db.c4byg4s6wmxz.us-east-1.rds.amazonaws.com',
+    DB_USER: 'admin',
+    DB_PWD: 'Salem1234**',
+    DB_DATABASE: 'webappdb'
+});
